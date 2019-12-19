834b0dd1baf17f47f96fa1ed3c6d56747b1a4d0b
JakubPavlicek <56623864+JakubPavlicek@users.noreply.github.com>
2019-12-19 11:04:59 +0100

    Update task03.cpp

--- task03.cpp
+++ task03.cpp
@@ -17,48 +17,72 @@
 #include <cstdio>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int vf(int argc, char* argv[]){
 
-  char fileCont[255];   // proměnná char mi definuje charaktery, složená závorka mi nastavuje velikost charakterů na 255
+  char fileCont[255];   
   int i;                
-  int vysledek = 0;     // proměnná int mi definuje celá čísla  
+  int vysledek = 0;     
   int cisla[80];
-  bool verbose = 0;     // proměnná bool mi definuje buď 0 nebo 1
-                        
+  bool verbose = 1;
 
-  if(strcmp(argv[1], "-f") == 0) {    // pokud se argv[1] a "-f" rovnají funkce vrací 0
-    FILE *fp = fopen(argv[2], "r");   // otevře soubor jenom pro čtení
+  if(strcmp(argv[1], "-v") == 0 && strcmp(argv[2], "-f") == 0){
+    FILE *fp = fopen(argv[3], "r");
+    verbose;
 
-    if(strcmp(argv[3], "-v") == 0) {
-      verbose = 1;
-    }
-    else {
-      verbose;
-    }
-
-    while(fscanf(fp, "%s", fileCont) != EOF) // funkce fscanf čte ze souboru, v tomto případě čte text
-    {
-      sscanf(fileCont,"%d",&i); // funkce sscanf v tomto případě načítá čísla ze souboru
+    do{
+      sscanf(fileCont, "%d", &i); // funkce sscanf v tomto případě načítá čísla ze souboru
       vysledek += i;
     }
-    if(verbose == 0)  
-      printf("%d", vysledek);
-    else{
-      printf("sum of numbers in %s is %d", argv[2], vysledek);
-    }
+
+    while(fscanf(fp, "%s", fileCont) != EOF); // funkce fscanf čte ze souboru, v tomto případě čte text
+
   }
 
+  printf("\b\bsum of numbers in %s is %d", argv[3], vysledek);
+
+  return 0;
+}
+
+int v(int argc, char* argv[]){
+
+    char fileCont[255];   // proměnná char mi definuje charaktery, složená závorka mi nastavuje velikost charakterů na 255
+    int i;                
+    int vysledek = 0;     // proměnná int mi definuje celá čísla  
+    int cisla[80];
+    bool verbose = 1;
+
+    if(strcmp(argv[3], "-v") == 0){     // pokud se argv[1] a "-f" rovnají funkce vrací 0
+      FILE *fp = fopen(argv[2], "r");   // otevře soubor, který bude na 3. pozici v konzoli (protože argv[1] je 2. pozice a na argv[0] je task03) jenom pro čtení
+      verbose;
+
+    do{
+      sscanf(fileCont, "%d", &i); 
+      vysledek += i;
+    }
+
+    while(fscanf(fp, "%s", fileCont) != EOF); 
+
+  }
+
+  printf("\b\bsum of numbers in %s is %d", argv[2], vysledek);
+
+  return 0;
+}
+
+int inr(int argc, char* argv[]){
+  //    SEM PATRI KOD TASK03 -I -V
+
+  char fileCont[255];   
+  int i;                
+  int vysledek = 0;     
+  int cisla[10];
+  bool verbose = 0; 
 
   if(strcmp(argv[1], "-i") == 0) {
     int pocetCisel = 0;
-    if(strcmp(argv[2], "-v") == 0) {
-      verbose = 1;
-    }
-    else{
-      verbose;
-    }
+    verbose;
 
-    for(int x = 0; x < 80; x++){
+    for(int x = 0; x < 10; x++){
       scanf("%d", &cisla[x]); // funkce scanf udělá to, že uživatel mi dosadí číslo do x
       if(getchar() != 'e') {  // funkce getchar() mi způsobí přerušení smyčky pomocí charakteru 'e'
         vysledek += cisla[x]; // operátor += sčítá
@@ -69,13 +93,72 @@ int main(int argc, char* argv[]) {
         break;
       }
     }
-    if(verbose == 0){
+      if(verbose == 0){
       printf("%d", vysledek);
     }
-    else{
-      printf("sum of %d numbers from standard input is %d", pocetCisel, vysledek);  // printf v tomto případě vypíše pocetCisel a vysledek, přičemž výsledek
-    }                                                                               // součet zadaných čísel uživatelem
+    if(strcmp(argv[2], "-v") == 0){
+      printf("\b\bsum of %d numbers from standard input is %d", pocetCisel, vysledek);
+    }
+    }
+    else if(strcmp(argv[2], "-v") == 0) {
+      int pocetCisel = 0;
+      verbose = 1;
+
+    for(int x = 0; x < 10; x++){
+      scanf("%d", &cisla[x]); 
+      if(getchar() != 'e') {  
+        vysledek += cisla[x]; 
+        pocetCisel++; 
+        continue;
+      }
+      else{
+        break;
+      }
+    }
+
   }
 
   return 0;
+}
+
+int main(int argc, char* argv[]) {
+
+    char fileCont[255];  
+    int i;                
+    int vysledek = 0;      
+    int cisla[80];
+    bool verbose = 0;
+
+    if(strcmp(argv[1], "-f") == 0){     
+      FILE *fp = fopen(argv[2], "r");   
+      verbose;
+
+    do{
+      FILE *fp = fopen(argv[2], "r");
+      sscanf(fileCont, "%d", &i); 
+      vysledek += i;
+    }
+
+    while(fscanf(fp, "%s", fileCont) != EOF); 
+
+  }
+
+  if(strcmp(argv[1], "-f") == 0){  
+    printf("%d", vysledek);
+  }
+
+  if(strcmp(argv[3], "-v") == 0){
+    v(argc, argv);
+  }
+
+  if(strcmp(argv[1], "-v") == 0){
+    vf(argc, argv);
+  }
+
+  if(strcmp(argv[1], "-i") == 0){
+    inr(argc, argv);
+  }
+
+  return 0;
+
 }
