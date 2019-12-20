0a54db58f576dbc38ca7ef13e0b5a9df0715183e
JakubPavlicek <56623864+JakubPavlicek@users.noreply.github.com>
2019-12-20 22:16:53 +0100

    Update task04.cpp

--- task04.cpp
+++ task04.cpp
@@ -8,45 +8,12 @@
 using namespace std;
 
 struct Animal {
-  char name[50];
-  char whatDoesItSay[50];
-};
-
-int main() {
-  
-  struct Animal Dog;
-  strcpy(Dog.name, "dog");
-  strcpy(Dog.whatDoesItSay, "woof");
-
-  struct Animal Fox;
-  strcpy(Fox.name, "fox");
-  strcpy(Fox.whatDoesItSay, "ringidingidingidingiding");
-
-  printf("Animal %s says %s\n", Dog.name, Dog.whatDoesItSay);
-  printf("Animal %s says %s\n", Fox.name, Fox.whatDoesItSay);
-  // dog says woof
-  // fox says ringidingidingidingiding
-}
-
-// DALŠÍ POKUS O FUNKČNOST, AVŠAK TENTO KÓD DOLE MI PÍŠE ERROR undefined reference to `vtable for Animal' A NEVÍM PROČ
-
-/*
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdio>
-
-using namespace std;
-
-struct Animal {
 
   char buffer[48];
   const char* name;
   const char* say;
-  virtual const char* whatDoesItSay();
-  // TODO
+  virtual void whatDoesItSay() = 0;
+
     Animal(const char* name, const char* say){
     this->name = name;
     this->say = say;
@@ -54,24 +21,20 @@ struct Animal {
 };
 
 struct Dog : Animal {
-  // TODO
-  
+
   Dog(const char* name, const char* say) : Animal(name, say){};
 
-  const char* whatDoesItSay(){
-    sprintf(buffer, "Zvire %s rika %s", name, say);
-    return buffer;
+  void whatDoesItSay() {
+    cout << "dog rika " << this->say << endl;
   }
 };
 
 struct Fox : Animal {
-  // TODO
    
   Fox(const char* name, const char* say) : Animal(name, say){};
 
-  const char* whatDoesItSay(){
-    sprintf(buffer, "Zvire %s rika %s", name, say);
-    return buffer;
+  void whatDoesItSay() {
+    cout << "liska rika " << this->say <<endl;
   }
 };
 
@@ -85,6 +48,3 @@ Animal* animals[] = {
     animal->whatDoesItSay();
   }
 }
-  // dog says woof
-  // fox says ringidingidingidingiding
-*/
