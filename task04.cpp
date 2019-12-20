6d77f0b39dc34c1c758883a5ee66686cc6359f22
JakubPavlicek <56623864+JakubPavlicek@users.noreply.github.com>
2019-12-20 20:32:50 +0100

    Update task04.cpp

--- task04.cpp
+++ task04.cpp
@@ -27,3 +27,64 @@ int main() {
   // dog says woof
   // fox says ringidingidingidingiding
 }
+
+// DALŠÍ POKUS O FUNKČNOST, AVŠAK TENTO KÓD DOLE MI PÍŠE ERROR undefined reference to `vtable for Animal' A NEVÍM PROČ
+
+/*
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+struct Animal {
+
+  char buffer[48];
+  const char* name;
+  const char* say;
+  virtual const char* whatDoesItSay();
+  // TODO
+    Animal(const char* name, const char* say){
+    this->name = name;
+    this->say = say;
+  }
+};
+
+struct Dog : Animal {
+  // TODO
+  
+  Dog(const char* name, const char* say) : Animal(name, say){};
+
+  const char* whatDoesItSay(){
+    sprintf(buffer, "Zvire %s rika %s", name, say);
+    return buffer;
+  }
+};
+
+struct Fox : Animal {
+  // TODO
+   
+  Fox(const char* name, const char* say) : Animal(name, say){};
+
+  const char* whatDoesItSay(){
+    sprintf(buffer, "Zvire %s rika %s", name, say);
+    return buffer;
+  }
+};
+
+int main() {
+
+Animal* animals[] = {
+    new Dog("dog", "woof"),
+    new Fox("fox", "ringidingidingidingiding")
+  };
+  for(Animal* animal : animals) {
+    animal->whatDoesItSay();
+  }
+}
+  // dog says woof
+  // fox says ringidingidingidingiding
+*/
