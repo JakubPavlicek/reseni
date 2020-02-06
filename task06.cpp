5d77252c217d69ab5fd092e018a32414fe222546
Jakub Pavlíček <56623864+JakubPavlicek@users.noreply.github.com>
2020-02-06 22:44:02 +0100

    Add files via upload

---
+++ task06.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class TellDamage{ 
+public:
+    virtual string Tell()=0;
+};
+
+class Ninja : public TellDamage{
+public:
+    string Tell(){
+        return "Ninja ti udelil 20 damage";
+    }
+
+};
+
+class Monster : public TellDamage{
+public:
+    string Tell(){
+       return "Monster ti udelil 50 damage";
+    }
+
+};
+
+class Enemy{
+public:
+    TellDamage* tell;
+
+    Enemy(TellDamage* damage){
+        this->tell = damage;
+    }
+    string Tell(){
+        return this->tell->Tell();
+    };
+};
+
+int main(){
+
+    Enemy* ninja = new Enemy(new Ninja);
+    Enemy* monster = new Enemy(new Monster);
+
+    cout << ninja->Tell() << "\n";
+    cout << monster->Tell() << "\n";
+}
